62e49d23ef9bb37a9d5c0f3675f2c78d6091b8c6
Omid <omid@sabz.city>
2022-04-17 12:46:18 +0430

    [person-authentication] Add two related domain

---
+++ protocol/person-otp.go
@@ -0,0 +1,11 @@
+/* For license and copyright information please see LEGAL file in repository */
+
+package pa
+
+// PersonOTP indicate the person-otp domain record data fields.
+type PersonOTP interface {
+	PersonID() [16]byte   // user-status domain
+	OTPPattern() []byte   // https://tools.ietf.org/html/rfc6238
+	OTPAdditional() int32 // easy to remember and must be 2 to 7 digit. https://en.wikipedia.org/wiki/Personal_identification_number
+	RequestID() [16]byte  // user-request domain
+}
---
+++ protocol/person-password.go
@@ -0,0 +1,21 @@
+/* For license and copyright information please see LEGAL file in repository */
+
+package org
+
+// PersonPassword indicate the person-password domain record data fields.
+type PersonPassword interface {
+	PersonID() [16]byte // user-status domain
+	PasswordHash() []byte
+	Status() PersonPasswordStatus
+	RequestID() [16]byte // user-request domain
+}
+
+// PersonPasswordStatus indicate PersonPassword record status
+type PersonPasswordStatus uint8
+
+// PersonPasswordStatus status
+const (
+	PersonPasswordStatus_Unset              PersonPasswordStatus = 0
+	PersonPasswordStatus_ForceUse2Factor    PersonPasswordStatus = (1 << iota) // authenticate person with Password + OTP
+	PersonPasswordStatus_MustChangePassword                                    // user must change password and otp key(if enabled) for some reason
+)
