952aac6d42651f25a424bfdbbb5d099f7f1ea6d7
Omid <omid@sabz.city>
2022-05-05 17:29:40 +0430

    [category] Initialize with two domains

---
+++ protocol/category.go
@@ -0,0 +1,24 @@
+/* For license and copyright information please see LEGAL file in repository */
+
+package org
+
+import (
+	"../libgo/protocol"
+)
+
+// Category or Topic
+type Category interface {
+	QuiddityID() [16]byte // CategoryID
+	ParentID() [16]byte   // CategoryID, Exist if it isn't top category.
+	Time() protocol.Time  // Save time
+	RequestID() [16]byte  // user-request domain
+}
+
+// TODO::: cache strategy?? 1 day or what?
+type Category_StorageServices interface {
+	Save(c Category) (err protocol.Error)
+
+	GetIDs(offset, limit uint64) (quiddityIDs [][16]byte, numbers uint64, err protocol.Error)
+
+	Delete(quiddityID [16]byte) (err protocol.Error)
+}
